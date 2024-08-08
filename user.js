f6aed75861a0a933dc99428e04b00c3c1465e573
Zach Dodson <zdodson21@gmail.com>
2024-08-08 09:37:52 -0400

    added some prefs

--- user.js
+++ user.js
@@ -1,1 +1,30 @@
+// Enable HTTPS-only mode
+user_pref("dom.security.https_only_mode", true);
 
+// Disable Pocket
+user_pref("browser.pocket.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+
+// Only Active Tab Has Close Button
+user_pref(browser.tabs.tabClipWidth, 999);
+
+// Disable Middle Mouse Paste
+user_pref(middlemouse.paste, false);
+
+// Show Compact Mode Setting
+user_pref("browser.compactmode.show", true);
+
+// Disable Notification
+user_pref("dom.webnotifications.enabled", false);
+
+// Disable Tabs after 30 Seconds
+user_pref("browser.tabs.min_inactive_duration_before_unload", 30000);
+
+// Disable Unneccessary Suggestions
+
+
+// Allow custom CSS
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Load MMB Tab in Background
+user_pref("browser.tabs.loadInBackground", true);
